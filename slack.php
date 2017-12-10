e7bec2624719dfcd4ad2e2fb4fb84d46693c31a8
Vijayaragavan <vijs@adobe.com>
2017-12-10 19:51:38 +0530

    added

---
+++ slack.php
@@ -0,0 +1,33 @@
+<?php
+
+error_reporting( E_ALL );
+
+        $data = array(
+            'text' => 'test post from script by Drtuts.com'
+        );
+
+
+$json_string = json_encode($data);
+
+$slack_webhook_url = 'https://hooks.slack.com/services/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';
+
+$slack_call = curl_init();
+curl_setopt($slack_call, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($slack_call, CURLOPT_POSTFIELDS, $json_string);
+curl_setopt($slack_call, CURLOPT_CRLF, true);
+curl_setopt($slack_call, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($slack_call, CURLOPT_URL, $slack_webhook_url);
+curl_setopt($slack_call, CURLOPT_SSL_VERIFYPEER, false);
+
+curl_setopt($slack_call, CURLOPT_HTTPHEADER, array(
+    "Content-Type: application/json",
+    "Content-Length: " . strlen($json_string))
+);
+
+$result = curl_exec($slack_call);
+
+print_r( "Posted successfully");
+
+curl_close($slack_call);
+
+ ?>
